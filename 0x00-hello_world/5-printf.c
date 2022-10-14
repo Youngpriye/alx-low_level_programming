19c568dd5f5de79bf4023b35b6e3834d8de06f8b
Youngpriye <priyeorinaemi@gmail.com>
2022-10-14 08:08:37 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - String to be printed
+ * Description: Ensure you use printf
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
