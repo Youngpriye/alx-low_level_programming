d39bfb2161dd6537ae5f828800c1c1d5d82760cf
Youngpriye <priyeorinaemi@gmail.com>
2022-10-14 20:04:15 -0700

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Character size should be printed
+ * Description: To print the various size of a computer
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("size of a char: %zu byte(s)\n", sizeof(char));
+	printf("size of an int: %zu byte(s)\n", sizeof(int));
+	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %zu byte(s)\n", sizedof(long long int));
+	printf("size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
