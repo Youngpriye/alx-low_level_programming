2f641c1c2ca6878dcbc63eac7a863d8afb2455f6
Youngpriye <priyeorinaemi@gmail.com>
2022-10-17 21:57:59 -0700

    printalphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 sucess
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'Z') {
+			puthcar(c);
+			c++;
+		}
+	while 
+		(d <=  'z') {
+			putcher(c);
+			d++;
+		}
+	putchar('\n')
+	return (0);
