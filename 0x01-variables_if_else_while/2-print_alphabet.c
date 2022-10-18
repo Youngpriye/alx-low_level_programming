1073fcdfeb6e304a04fc5cba9c8e63f02d6e5a9b
Youngpriye <priyeorinaemi@gmail.com>
2022-10-17 21:09:50 -0700

    Print_Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Point of entry
+ * Return: 0 always
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a' ; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n");
+	return (0);
+}
