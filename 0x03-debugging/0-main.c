74bda0e3f76e28194b4f9e3eaace89a07db8aafc
Youngpriye <priyeorinaemi@gmail.com>
2022-10-21 20:02:00 -0700

    positive_or_negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - To test for positive or negative function
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+A readmefile for dubbuging in C program
