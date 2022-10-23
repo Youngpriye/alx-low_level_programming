fc46d0ea305e2ec1761c52e9a5c90ede3f0c674b
Youngpriye <priyeorinaemi@gmail.com>
2022-10-23 16:01:48 -0700

    print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point for integers
+ * Description: Prints base 10 digits
+ * Return: 0 sucess
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x < 10 ; x++)
+	{
+		printf("%d", x);
+	}
+	putchar('\n');
+	return (0);
+}
