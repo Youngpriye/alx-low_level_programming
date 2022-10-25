b90a9cf12ad14059d7e458740c04fb9006ddce2d
Youngpriye <priyeorinaemi@gmail.com>
2022-10-24 21:40:03 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - randomly generate passwords
+ * Description: To generate valid random passwords
+ * Return: void
+ */
+
+int main(void)
+{
+	int sum;
+	char p;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		p = rand() % 128;
+		sum += p;
+		putchar(p);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
