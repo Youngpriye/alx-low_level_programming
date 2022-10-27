9bfaa7b00f869388746c687f23187bd44009fad8
Youngpriye <priyeorinaemi@gmail.com>
2022-10-27 11:27:05 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: input string to make 1337
+ * Return: the point s
+ */
+char *leet(char *s)
+{
+	int x, y;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (s[x] == s1[y])
+			{
+				s[x] = s2[y];
+			}
+		}
+	}
+	return (s);
+}
