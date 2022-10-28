bb45719864aa093d67035364040bb184aa61c1d1
Youngpriye <priyeorinaemi@gmail.com>
2022-10-27 20:46:10 -0700

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,25 +1,30 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * cap_string - it capitalizes words of a string
- * @s: string to be processed
- * Return: pointer to string
+ * cap_string - Words of a spring to be capitalize
+ * @s: input string.
+ * Return: the pointer to dest.
  */
+
 char *cap_string(char *s)
 {
-	int i, j;
-	char delimeters[] = " \t\n,;.!?\"(){}";
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	for (i = 0; s[i] != '\0'; i++)
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		if (s[0] >= 97 && s[0] <= 122)
-			s[0] = s[0] - 32;
-	}
-	for (j = 0; delimeters[j] != '\0'; j++)
-	{
-		if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
-			s[i + 1] = s[i + 1] - 32;
+		for (i = 0; i < 13; i++)
+		{
+			if (*(s + count) == sep_words[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
+		}
+		count++;
 	}
 	return (s);
 }
