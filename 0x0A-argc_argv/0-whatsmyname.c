c26d7a0ec39fe4f526835c6f407ffdbcfdfd7266
Youngpriye <priyeorinaemi@gmail.com>
2022-11-07 05:17:44 -0800

    agrc agrv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+/**
+ * main - this is a function to print its name
+ * @argc: size of argv
+ * @argv: an array of a command listed
+ * Return: Alway 0 (sucess)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
